60990b4c172652fecd1121668389efe28f8f2670
mchaudhury <mushsalcha@gmail.com>
2021-10-20 17:55:26 -0500

    added first java program

---
+++ src/com/titlecase/Main.java
@@ -0,0 +1,41 @@
+package com.titlecase;
+import javax.swing.*;
+import java.awt.*;
+
+public class Main {
+    public static void main(String... args){
+        JFrame application = createGUI();
+        application.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        application.setVisible(true);
+    }
+    private static JFrame createGUI() {
+        JTextField input = new JTextField();
+        input.setPreferredSize(new Dimension(300, 40));
+        JButton convertButton = new JButton("convert");
+        JLabel output = new JLabel();
+        output.setPreferredSize(new Dimension(300, 40));
+
+        /* Statement lambda */
+/*
+        convertButton.addActionListener((event -> {
+            output.setText(TitlecaseConverter.convertToTitleCase(input.getText()));
+        });
+
+ */
+        /* expression lambda */
+        convertButton.addActionListener((event) -> output.setText(TitlecaseConverter.convertToTitleCase(input.getText()))
+        );
+
+        JFrame gui = new JFrame("Title Case Converter");
+        gui.setLayout(new FlowLayout());
+        gui.add(input);
+        gui.add(convertButton);
+        gui.add(output);
+        gui.pack();
+        gui.setLocationRelativeTo(null); // Centering the screen
+
+        return gui;
+
+    }
+    
+}
