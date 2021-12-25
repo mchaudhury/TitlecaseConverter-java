43f66c74b5a2db3a769f8660c572e8bbf7919b03
mchaudhury <mushsalcha@gmail.com>
2021-12-25 10:54:08 -0600

    update

--- src/com/titlecase/Main.java
+++ src/com/titlecase/Main.java
@@ -14,6 +14,12 @@ public class Main {
         JButton convertButton = new JButton("convert");
         JLabel output = new JLabel();
         output.setPreferredSize(new Dimension(300, 40));
+
+        JTextField input = new JTextField();
+        input.setPreferredSize(new Dimension(300, 40));
+        JButton convertButton = new JButton("convert");
+        JLabel output = new JLabel();
+        output.setPreferredSize(new Dimension(300, 40));
         
 
         /* Statement lambda */
